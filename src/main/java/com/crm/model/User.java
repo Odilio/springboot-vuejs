bc7f2382184fed051acc51459395c64a402ad5fb
Teste <odilionoronha@gmail.com>
2019-07-26 16:13:05 -0300

    incial comit

---
+++ src/main/java/com/crm/model/User.java
@@ -0,0 +1,52 @@
+package com.crm.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_table")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Transient
+    private String passwordConfirm;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+ 
+}
---
+++ src/main/java/com/crm/repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.crm.repository;
+import com.crm.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
