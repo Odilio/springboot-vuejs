a8f48e94f46323413495d252cc30488b5b21171e
Odilio <odilionoronha@gmail.com>
2019-07-31 09:05:34 -0300

    adicionado exemplo de cadastro, listar e retorno de web token em vue js

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -3,13 +3,13 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Vue Axios Post - ItSolutionStuff.com</div>
+                    <div class="card-header">Cadastro de usuarios</div>
     
                     <div class="card-body">
                         <form @submit="formSubmit">
                         <strong>Name:</strong>
                         <input type="text" class="form-control" v-model="name">
-                        <strong>Description:</strong>
+                        <strong>Senha:</strong>
                         <textarea class="form-control" v-model="description"></textarea>
     
                         <button class="btn btn-success">Submit</button>
@@ -19,6 +19,28 @@
                         {{output}}
                         </pre>
                     </div>
+                    <div class="card-header">Login</div>
+
+                    <div class="card-body">
+
+                        <form @submit="formLogar">
+                        <strong>Name:</strong>
+                        <input type="text" class="form-control" v-model="username">
+                        <strong>password:</strong>
+                        <textarea class="form-control" v-model="password"></textarea>
+    
+                        <button class="btn btn-success">Submit</button>
+                        </form>
+                        <strong>Token:</strong>
+                        <pre>
+                        {{token}}
+                        </pre>
+                    </div>
+                    <ul >
+                        <li v-for="user in users"  :key="user.id">
+                        {{user}}
+                        </li>
+                    </ul>    
                 </div>
             </div>
         </div>
@@ -27,20 +49,27 @@
      
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-            this.axios.get('http://localhost:8089/questions')
-                .then(function (response) {
-                    console.log(response.data);
-                });
-        },
         data() {
             return {
+              users: [],
               name: '',
               description: '',
-              output: ''
+              output: '',
+              token: '',
+              password: '',
+              username: ''
             };
         },
+        mounted() {
+            console.log('Component mounted.')
+             let currentObj = this;
+            this.axios.get('http://localhost:8089/questions')
+                .then(function (response) {
+                    currentObj.users = response.data
+                    console.log(response.data)
+                });
+        },
+       
         methods: {
             formSubmit(e) {
                 
@@ -55,6 +84,21 @@
                 })
                 .catch(function (error) {
                     currentObj.output = error;
+                });
+            },
+             formLogar(e) {
+                
+                e.preventDefault();
+                let currentObj = this;
+                this.axios.post('http://localhost:8089/authenticate', {
+                    username: this.username,
+                    password: this.password
+                })
+                .then(function (response) {
+                    currentObj.token = response.data;
+                })
+                .catch(function (error) {
+                    currentObj.token = error;
                 });
             }
         }
--- src/main/java/com/crm/CrmApplication.java
+++ src/main/java/com/crm/CrmApplication.java
@@ -28,7 +28,8 @@ public class CrmApplication {
         CorsConfiguration config = new CorsConfiguration();  
         config.setAllowCredentials(true); 
         // *** URL below needs to match the Vue client URL and port ***
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:8081")); 
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); 
         config.setAllowedMethods(Collections.singletonList("*"));  
         config.setAllowedHeaders(Collections.singletonList("*"));  
         source.registerCorsConfiguration("/**", config);  
